3c7d038c15cdeaaa02e694a537993e96664727cf
Luiz Carlos <lucachaves@gmail.com>
2013-05-24 01:10:14 -0300

    update testcase

--- src/test/util/NumberConverterTest.php
+++ src/test/util/NumberConverterTest.php
@@ -2,42 +2,65 @@
 require_once __DIR__."/../../app/util/NumberConverter.php";
 
 /**
- * TestCaste to NumberConvert
+ * TestCaste to util NumberConvert
  */
 class NumberConverterTest extends PHPUnit_Framework_TestCase
 {
 
-	  public function setUp()
-    {
-    }
+  public function setUp()
+  {
+  }
 
-    /**
-     * @dataProvider provider
-     */
-    public function testDigitsToRoman($number, $roman)
-    {
-      	$this->assertEquals($roman, digitsToRoman($number));
-    }
+  /**
+    * @expectedException InvalidArgumentException
+    */
+  public function testThrowsExceptionIfNonNumberIsPassed()
+  {
+    digitToRoman('a');
+  }
 
-    public function provider()
-    {
-        return array(
-          array(1, "I"),
-          array(10, "X"),
-          array(16, "XVI"),
-          array(55, "LV"),
-          array(95, "XCV"),
-          array(100, "C"),
-          array(266, "CCLXVI"),
-          array(500, "D"),
-          array(1000, "M"),
-          array(2555, "MMDLV")
-        );
-    }
+  /**
+   * Test of the digitToRoman function
+   * @dataProvider provider
+   */
+  public function testDigitToRoman($digit, $roman)
+  {
+    $this->assertEquals($roman, digitToRoman($digit));
+  }
 
-    public function tearDown()
-    {
-    }
+  /**
+   * Test of the romanToDigit function
+   * @dataProvider provider
+   */
+  public function testRomanToDigit($digit, $roman)
+  {
+    $this->assertEquals($digit, romanToDigit($roman));
+  }
+
+  /**
+   * Dataprovider to test of the digitToRoman and romanToDigit functions
+   */
+  public function provider()
+  {
+    return array(
+      array(1, "I"),
+      array(10, "X"),
+      array(16, "XVI"),
+      array(55, "LV"),
+      array(95, "XCV"),
+      array(100, "C"),
+      array(266, "CCLXVI"),
+      array(500, "D"),
+      array(1000, "M"),
+      array(2555, "MMDLV")
+    );
+  }
+
+
+
+  public function tearDown()
+  {
+  }
 
 }
 
